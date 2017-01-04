8c041e7e48b3fb68b8ccadd162ee3f03fb23c7a2
Lise Poirier <lise.p.poirier@gmail.com>
2017-01-04 13:57:38 +0100

    Slide present

--- present-slide/js/script.js
+++ present-slide/js/script.js
@@ -33,3 +33,18 @@ $(document).ready(function() {
    $('.button').keydown(function(){
        $("body").animate({ scrollTop: window.pageYOffset + $(window).height()}, 800);
    });
+
+
+$("body").addClass("thin");
+// If user has Javascript disabled, the thick scrollbar is shown
+$("body").mouseover(function(){
+  $(this).removeClass("thin");
+});
+$("body").mouseover(function(){
+  $(this).addClass("thin");
+});
+$("body").scroll(function () {
+  $("body").addClass("thin");
+});
+
+// Using http://css-tricks.com/custom-scrollbars-in-webkit/ for basic custom scrollbars
